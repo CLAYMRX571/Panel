5d70dc9c9be9a2608e6b066ac2d4482e4ce4515d
CLAYMRX571 <claymrx571@gmail.com>
2025-08-22 22:06:10 +0500

    Hummers

--- apps/products/urls.py
+++ apps/products/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import HomesView, Lang
+from .views import HomesView, lan_switch
 
 urlpatterns = [
     path("", HomesView.as_view(), name="index"),
-    path("lan/<str:lan>/", Lang.as_view(), name="lan_switch"),
+    path("lan/<str:lan>/", lan_switch, name="lan_switch"),
 ]
--- apps/products/views.py
+++ apps/products/views.py
@@ -1,11 +1,9 @@
 from django.shortcuts import render, redirect
 from django.views.generic import View
-from .models import Indexs
 
 class HomesView(View):
     def get(self, request):
         return render(request, "index.html") 
 
-class Lang(View):
-    def get(self, request, lan):
-        return redirect(f'/{lan}/')
+def lan_switch(request, lan):
+    return redirect(f'/{lan}/')
