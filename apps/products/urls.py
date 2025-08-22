e317554b66d9dc4e14103474ea2ddf76a2caa5d5
CLAYMRX571 <claymrx571@gmail.com>
2025-08-22 21:50:19 +0500

    sorry

--- apps/products/urls.py
+++ apps/products/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.HomesView, name='index'),
-    path('lan/<str:lan>/', views.lan_switch, name='lan_switch'),
+    path("", views.HomesView, name="index"),
+    path("lan/<str:lan>/", views.lan_switch, name="lan_switch"),
 ]
