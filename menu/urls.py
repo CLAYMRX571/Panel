b549b2532bedb982a130337b632a3ee29c3e60b6
CLAYMRX571 <claymrx571@gmail.com>
2025-08-22 21:40:02 +0500

    lang

--- menu/urls.py
+++ menu/urls.py
@@ -21,14 +21,11 @@ from django.conf.urls.static import static
 from django.utils.translation import gettext_lazy as _
 from django.conf.urls.i18n import i18n_patterns
 
-urlpatterns = [
-    path('i18n/', include('django.conf.urls.i18n')),
-]
-
-urlpatterns += i18n_patterns(
+urlpatterns =+ i18n_patterns[
     path('admin/', admin.site.urls),
     path('', include('apps.products.urls')),
-)
+    path('i18n/', include('django.conf.urls.i18n')),
+]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
