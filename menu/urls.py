af1a1bd389e9e77238b8a4daf8353245d78611ca
CLAYMRX571 <claymrx571@gmail.com>
2025-08-22 22:20:42 +0500

    ss

--- menu/urls.py
+++ menu/urls.py
@@ -26,5 +26,6 @@ urlpatterns = i18n_patterns(
     path('i18n/', include('django.conf.urls.i18n')),
 )
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
